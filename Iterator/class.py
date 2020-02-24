6c42e0475b6638ede330b61c04552b2065164b43
takaaki02 <takagikousuke02@gmail.com>
2020-02-24 13:14:24 +0900

    メソッドから値が取得できないバグの修正。 実行結果の追加。

--- Iterator/class.py
+++ Iterator/class.py
@@ -2,10 +2,9 @@ from interface import *
 
 
 class Subject(Aggregate):
-    students = list()
-    last = int()
 
     def __init__(self):
+        self.students = list()
         self.last = 0
 
     def get_student(self, index):
@@ -18,35 +17,46 @@ class Subject(Aggregate):
     def get_number_of_students(self):
         return self.last
 
-    @classmethod
     def iterator(self):
-        return SubjectIterator()
+        return SubjectIterator(self)
 
 
 class SubjectIterator(Iterator):
-    subject = Subject()
-    index = int()
-
-    def __init__(self):
+    def __init__(self, sub):
+        self.subject = sub
         self.index = 0
 
-    @classmethod
-    def has_next(cls):
-        if cls.index < cls.subject.get_number_of_students():
+    def has_next(self):
+        if self.index < self.subject.get_number_of_students():
             return True
 
         return False
 
-    @classmethod
-    def next(cls):
-        student = cls.subject.get_student(cls.index)
-        cls.index = cls.index + 1
-        return student
+    def next(self):
+        stu = self.subject.get_student(self.index)
+        self.index = self.index + 1
+        return stu
+
+
+class Student():
+    name = int()
+    student_number = int()
+
+    def __init__(self, name, number):
+        self.name = name
+        self.student_number = number
 
 
 if __name__ == '__main__':
-    ss = Subject()
-    ss.add_student('tt')
-    tt = ss.iterator()
-    print(tt.next())
-
+    student = Student('Sirajira Sajiki', 1)
+    student2 = Student('Sirajira Sajiki2', 2)
+    subject = Subject()
+    subject.add_student(student)
+    subject.add_student(student2)
+    print(subject.last)
+    iterator = subject.iterator()
+    print(iterator.has_next())
+    while iterator.has_next():
+        next = iterator.next()
+        print(next.name)
+        print(next.student_number)
